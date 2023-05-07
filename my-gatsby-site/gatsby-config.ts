fb9965be9a7a499c0ed54aa9b0341b9d20b9e9fb
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-05-07 14:31:57 +0800

    Update gatsby-config

--- my-gatsby-site/gatsby-config.ts
+++ my-gatsby-site/gatsby-config.ts
@@ -2,8 +2,9 @@ import type { GatsbyConfig } from "gatsby"
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://yintc123.github.io/Gatsby/`,
   },
+  pathPrefix: "/Gatsby",
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
